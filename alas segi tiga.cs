58cb94be67c4bdd83dd6d205d09b3d455d651ab6
mwrkhaeriza <mawar011208@gmail.com>
2024-11-19 13:32:21 +0700

    Add files via upload
    project (penjumlahan, alas segitiga, piramida 1 dan 2 sisi)

---
+++ alas segi tiga.cs
@@ -0,0 +1,18 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+Console.Write("Masukkan nilai alas segitiga: ");
+double alas = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Masukkan nilai tinggi segitiga: ");
+double tinggi = Convert.ToDouble(Console.ReadLine());
+
+double luas = 0.5*alas*tinggi;
+
+Console.WriteLine("Luas segitiga adalah:"+luas);
+    }
+}
---
+++ penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+Console.Write("Masukkan angka pertama: ");
+int angka1= Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Masukkan angka kedua: ");
+int angka2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Masukkan angka ketiga: ");
+int angka3 = Convert.ToInt32(Console.ReadLine());
+
+int hasil = angka1 + angka2 + angka3;
+
+Console.WriteLine("Hasil penjumlahan:"+hasil);
+    }
+}
